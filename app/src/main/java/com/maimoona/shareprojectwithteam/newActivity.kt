f2b2af8fd9ee0b8744c602c9c5c3d10f2444e487
Samar.almh <samar.almh@gmail.com>
2020-12-29 09:24:07 +0300

    add button and new activity

---
+++ app/src/main/java/com/maimoona/shareprojectwithteam/newActivity.kt
@@ -0,0 +1,11 @@
+package com.maimoona.shareprojectwithteam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class newActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
